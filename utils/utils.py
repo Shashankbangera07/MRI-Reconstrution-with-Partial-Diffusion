39afb79ac2774888764784353c6ecd9959a4c895
Shashank C Bangera <124120586+Shashankbangera07@users.noreply.github.com>
2026-03-02 19:30:57 +0100

    Improve seed setup for reproducibility
    Refactor seed setup for reproducibility in PyTorch and CUDA.

--- utils/utils.py
+++ utils/utils.py
@@ -10,26 +10,26 @@ import random
 
 
 def setup_seed(seed):
-    #  下面两个常规设置了，用来np和random的话要设置
+    
     np.random.seed(seed)
     random.seed(seed)
 
-    os.environ["PYTHONHASHSEED"] = str(seed)  # 禁止hash随机化
+    os.environ["PYTHONHASHSEED"] = str(seed)  
     os.environ[
         "CUBLAS_WORKSPACE_CONFIG"
-    ] = ":4096:8"  # 在cuda 10.2及以上的版本中，需要设置以下环境变量来保证cuda的结果可复现
+    ] = ":4096:8"  
 
     torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)  # 多GPU训练需要设置这个
+    torch.cuda.manual_seed_all(seed) 
     torch.manual_seed(seed)
 
     torch.use_deterministic_algorithms(
         True
-    )  # 一些操作使用了原子操作，不是确定性算法，不能保证可复现，设置这个禁用原子操作，保证使用确定性算法
-    torch.backends.cudnn.deterministic = True  # 确保每次返回的卷积算法是确定的
-    torch.backends.cudnn.enabled = False  # 禁用cudnn使用非确定性算法
+    ) 
+    torch.backends.cudnn.deterministic = True  
+    torch.backends.cudnn.enabled = False  
     torch.backends.cudnn.benchmark = (
-        False  # 与上面一条代码配套使用，True的话会自动寻找最适合当前配置的高效算法，来达到优化运行效率的问题。False保证实验结果可复现。
+        False  
     )
 
 
@@ -48,7 +48,7 @@ def init_seeds(seed=0):
     torch.cuda.manual_seed_all(seed)
 
     if seed == 0:
-        torch.backends.cudnn.deterministic = True  # 固定卷积算法, 设为True会导致卷积变慢
+        torch.backends.cudnn.deterministic = True 
         torch.backends.cudnn.benchmark = False
 
 
